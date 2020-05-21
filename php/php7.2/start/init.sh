18a9e0038cbb4d084216854a2b7765b40c4bdd6e
Rocky <xiaoyan.jiang@reaone.cn>
2020-05-21 21:59:13 +0800

    fixed

--- php/php7.2/start/init.sh
+++ php/php7.2/start/init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p /ws/volumes/php-fpm7.2/log/php-fpm
-#touch error.log
+touch /ws/volumes/php-fpm7.2/log/php-fpm/error.log
 #touch php-fpm.log
 cp -rf ./conf /ws/volumes/php-fpm7.2
 
