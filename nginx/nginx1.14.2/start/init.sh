8410d9983f382ad6667a7900546c096bb31aa6dc
Rocky <xiaoyan.jiang@reaone.cn>
2020-05-25 11:22:16 +0800

    test nginx

---
+++ nginx/nginx1.14.2/start/init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p /ws/volumes/nginx1.14.2/logs
+cp -rf ./conf /ws/volumes/nginx1.14.2
+
+#docker-compose up -d
+
+
---
+++ nginx/nginx1.16.1/start/init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p /ws/volumes/nginx1.16.1/logs
+cp -rf ./conf /ws/volumes/nginx1.16.1
+
+#docker-compose up -d
+
+
