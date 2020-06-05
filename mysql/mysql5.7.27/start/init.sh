62f3c96461b59c5717a0601d15426ff33682bb7a
Rocky <xiaoyan.jiang@reaone.cn>
2020-06-05 18:49:34 +0800

    'fixed#mysql-error.log'

--- mysql/mysql5.7.27/start/init.sh
+++ mysql/mysql5.7.27/start/init.sh
@@ -4,6 +4,7 @@ APP_PATH=/ws/volumes
 APP_NAME=mysql5.7.27
 
 mkdir -p $APP_PATH/$APP_NAME/{data,log}
+touch $APP_PATH/$APP_NAME/log/error.log
 cp -rf ./conf.d $APP_PATH/$APP_NAME
 
 #docker-compose up -d
