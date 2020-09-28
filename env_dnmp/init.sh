ece5fb017c8f1338a83b3d6f0f931fe9eb89ab03
Rocky <rockyjiang21@outlook.com>
2020-09-28 19:14:30 +0800

    m

--- env_dnmp/init.sh
+++ env_dnmp/init.sh
@@ -2,6 +2,7 @@
 
 WS_PATH=/ws/volumes
 WWW_PATH=/ws/www
+ENV_PATH=/ws/env
 
 NGINX=nginx1.16.1
 PHP72=php-fpm7.2.27
@@ -10,19 +11,19 @@ REDIS=redis5.0.9
 RABBITMQ=rabbitmq3.8.4m
 
 mkdir -p $WWW_PATH
-chown nobody:nobody -R $WWW_PATH
+chown -R nobody:nobody $WWW_PATH
+
+mkdir -p $ENV_PATH
 
 mkdir -p $WS_PATH/$NGINX/{conf,log}
 /bin/cp -rf ./conf/nginx/* $WS_PATH/$NGINX/conf
 
 mkdir -p $WS_PATH/$PHP72/{conf,log}
 touch $WS_PATH/$PHP72/log/error.log
-chmod a+rwx $WS_PATH/$PHP72/log/error.log
 /bin/cp -rf ./conf/php/* $WS_PATH/$PHP72/conf
 
 mkdir -p $WS_PATH/$MYSQL/{data,log,conf.d}
 touch $WS_PATH/$MYSQL/log/error.log
-chmod a+rwx $WS_PATH/$MYSQL/log/error.log
 /bin/cp -rf ./conf/mysql/conf.d/* $WS_PATH/$MYSQL/conf.d
 
 mkdir -p $WS_PATH/$REDIS/{data,conf}
@@ -31,6 +32,12 @@ mkdir -p $WS_PATH/$REDIS/{data,conf}
 mkdir -p $WS_PATH/$RABBITMQ/{data,log,conf}
 /bin/cp -rf ./conf/rabbitmq/conf/* $WS_PATH/$RABBITMQ/conf
 
+
+chmod a+rwx $WS_PATH/$NGINX/log
+chmod a+rwx $WS_PATH/$PHP72/log/error.log
+chmod a+rwx $WS_PATH/$MYSQL/log/error.log
+chmod a+rwx $WS_PATH/$RABBITMQ/{data,log}
+chmod a+rwx $WS_PATH/$MYSQL/{data,log}
+chmod a+rwx $WS_PATH/$REDIS/data
+
 #docker-compose up -d
-
-
