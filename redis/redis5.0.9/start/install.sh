71a0828d21ded875811ffb0f2025ce7d15cb77fd
Rocky <hidespace@outlook.com>
2021-05-11 19:12:08 +0800

    m

--- redis/redis5.0.9/start/install.sh
+++ redis/redis5.0.9/start/install.sh
@@ -5,4 +5,11 @@ REDIS=redis5.0.9
 
 mkdir -p $VOL_PATH/$REDIS/{data,conf} && /bin/cp -rf ./conf/* $VOL_PATH/$REDIS/conf
 
+#启动服务
 #docker-compose -f docker-compose.yml up -d
+
+#停止服务
+#docker-compose -f docker-compose.yml stop
+
+#停止并删除服务
+#docker-compose -f docker-compose.yml down
