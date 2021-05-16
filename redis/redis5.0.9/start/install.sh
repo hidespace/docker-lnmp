6f1e5e63328d290023a05ef8691b0c21a77a2b9b
Rocky <rockyjiang21@outlook.com>
2021-05-16 16:45:57 +0800

    增加设置redis服务密码脚本

--- redis/redis5.0.9/start/install.sh
+++ redis/redis5.0.9/start/install.sh
@@ -5,6 +5,16 @@ REDIS=redis5.0.9
 
 mkdir -p $VOL_PATH/$REDIS/{data,conf} && /bin/cp -rf ./conf/* $VOL_PATH/$REDIS/conf
 
+
+# redis password
+PASS_DEFAULT=so7X@co9Wp
+PASS_REPLACE=
+
+if [[ $PASS_REPLACE != '' ]]; then
+    sed -i "s/$PASS_DEFAULT/$PASS_REPLACE" $VOL_PATH/$REDIS/conf/redis.conf
+fi
+
+
 #启动服务
 #docker-compose -f docker-compose.yml up -d
 
